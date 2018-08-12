0eb3b4b1310723ffeb1887520c416b64045ee3f8
Jakub Czerwiński <poliakustyczny@gmail.com>
2018-08-12 15:17:45 +0200

    Iterate files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import magic
 
 class Yaps:
     def setDirectory(self, directory):
@@ -8,9 +9,19 @@ class Yaps:
         else:
             raise FileNotFoundError('Directory not found')
 
+    def iterateFiles(self):
+        for filename in os.listdir(self.directory):
+            print('Znaleziono plik: ', filename)
+            self.checkIfImage(filename)
+
+    def checkIfImage(self, file):
+        mime = magic.Magic(mime=true)
+        print(mime.from_file(self.directory,'/',file))
+
 app = Yaps()
 
 if(len(sys.argv) > 1):
     app.setDirectory(sys.argv[1])
+    app.iterateFiles()
 else:
     raise Exception('Not enough parameters')
