b4debb761d6471bea8f665a895d2a3951a230338
Jakub Czerwiński <p.jakub.czerwinski@gmail.com>
2018-12-03 16:05:00 +0100

    working first test

--- tests/test_yaps.py
+++ tests/test_yaps.py
@@ -1,7 +1,28 @@
 import pytest
-# from yaps import Yaps
-class TestYaps:
+import os
 
-    def testSetDirectory(self):
-        yaps = Yaps()
-        yaps.setDirectory('.')
+from src.yaps.yaps import Yaps
+
+from pprint import pprint
+
+def testSetDirectory():
+    yaps = Yaps()
+
+    assert yaps.directory == False
+
+    yaps.setDirectory('.')
+    assert yaps.directory == '.'
+
+    with pytest.raises(FileNotFoundError):
+        yaps.setDirectory('./notExistingDir')
+
+def testSetOutputDirectory(tmpdir):
+    d = tmpdir.mkdir('unwriteableDir', '0666')
+    path = str(d)
+    yaps = Yaps()
+    yaps.setOutputDirectory(path)
+
+    pprint(str(d))
+    assert 0
+
+
